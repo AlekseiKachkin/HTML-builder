97e2dc2e5da4f988e4707027bb7580acbd0d8be1
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-24 22:13:29 +0300

    fix: delete result files

---
+++ 06-build-page/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+(async () => {
+    const files =  await fsPromises.readdir(path.join(__dirname, 'styles'), {withFileTypes: true}); 
+    for(let file of files) {
+        if (file.isFile() && file.name.split('.')[1] === 'css') {           
+            let src = path.join(__dirname, 'styles', file.name);
+            let dest = path.join(__dirname, 'project-dist', 'bundle.css');
+            const dataStyles = fs.createReadStream(src, 'utf-8');
+            dataStyles.on('data', (chunk) => {
+                fsPromises.appendFile(dest, chunk, 'utf-8')
+            });            
+        }
+    }  
+})()
