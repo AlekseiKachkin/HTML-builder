4f643d14e5f199c2436f4b44eb3dd74a6e30f1cb
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-24 11:07:06 +0300

    fix: fix mistake 'stat' method(add right path)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,16 +1,15 @@
 const fsPromises = require('fs/promises');
-
 const path = require('path');
     
 (async () => {
     const files =  await fsPromises.readdir(path.join(__dirname, 'secret-folder'), {withFileTypes: true}); 
     for(let file of files) {
         if (file.isFile()) {
-            const fullFileName = file.name.split('.');
-            const fileName = fullFileName[0];
-            const fileExtend = fullFileName[1];
-            const fileSize = await fsPromises.stat(__dirname, 'secret-folder', `${fullFileName}`);
-            console.log(`${fileName} - ${fileExtend} - ${fileSize.size}kb`);
+            const fullFileName = file.name;
+            const fileName = fullFileName.split('.')[0];
+            const fileExtend =fullFileName.split('.')[1];
+            const fileStats = await fsPromises.stat(path.join(__dirname, 'secret-folder', fullFileName));
+            console.log(`${fileName} - ${fileExtend} - ${fileStats.size}b`);
         }
     }
 })()
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+(function copyDir(){
 
+})()
