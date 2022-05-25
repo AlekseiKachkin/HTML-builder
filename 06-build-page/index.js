13071c87cad74a289afa830e20a30bb9aaf519ae
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-26 00:59:52 +0300

    feat: add copyDir Function for 06-build-page'

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,31 +2,42 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-async function copyDir (dirName)  {
-    fs.mkdir(path.join(__dirname,'project-dist', dirName) , {recursive: true}, (err) => {
-        if (err) throw err;
-      });
-    const srcDir =  await fsPromises.readdir(path.join(__dirname, dirName), {withFileTypes: true}); 
-    for(let obj of srcDir) {
-        let src = path.join(__dirname,  dirName, obj.name);
-        let dest = path.join(__dirname,'project-dist', dirName, obj.name);
-        console.log(src);
-        if (!obj.isDirectory()) { 
-            fsPromises.copyFile(src, dest);
+const distDir = path.join(__dirname, 'project-dist')
+const assetsSrc = path.join(__dirname, 'assets');
+const assetsDst = path.join(distDir, 'assets');
+
+async function makeDist () {
+   fs.mkdir(distDir, {recursive: true}, (err) => {
+    if (err) throw err;
+  });
+} 
+
+async function copyDir(srcDir, destDir) {
+
+    fs.mkdir(destDir, { recursive: true }, (err) => {
+        if (err)
+            throw err;
+    });
+    const distFiles = await fsPromises.readdir(destDir, { withFileTypes: true });
+    for (let obj of distFiles) { 
+        if (obj.isFile()){
+            await fsPromises.rm(path.join(destDir, obj.name));
+        }       
+    }
+    
+    const copyContent = await fsPromises.readdir(srcDir, { withFileTypes: true })
+    for (let obj of copyContent) {
+        if (obj.isFile()) {
+            fsPromises.copyFile(path.join(srcDir, obj.name), path.join(destDir, obj.name))
         } else if (obj.isDirectory()) {
-            fs.mkdir(path.join(__dirname, dest) , {recursive: true}, (err) => {
-                if (err) throw err;
-              });        
-            let track = `${dirName}/${obj.name}`
-            return copyDir(track);
+            let src = path.join(srcDir, obj.name)
+            let dest = path.join(destDir, obj.name)
+            copyDir(src, dest)
         }
-    }  
+    }
+
 }
 
-(async () => {
-    fs.mkdir(path.join(__dirname, 'project-dist') , {recursive: true}, (err) => {
-        if (err) throw err;
-      });
+makeDist();
+copyDir(assetsSrc, assetsDst);
 
-      copyDir ('assets');  
-})();
